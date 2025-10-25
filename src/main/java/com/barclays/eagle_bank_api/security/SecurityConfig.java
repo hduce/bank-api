9d0ecd2a84afcb914c2d0fb606efa31ecaea056b
Harry Duce <harry.duce@gmail.com>
2025-10-25 15:50:08 +0100

    move securityconfig to security/

--- src/main/java/com/barclays/eagle_bank_api/security/SecurityConfig.java
+++ src/main/java/com/barclays/eagle_bank_api/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.barclays.eagle_bank_api.config;
+package com.barclays.eagle_bank_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
