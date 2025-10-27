df658016592e977c3750fff5a5989d733c450588
Harry Duce <harry.duce@gmail.com>
2025-10-27 12:08:55 +0000

    add default switch case

--- src/main/java/com/barclays/eagle_bank_api/domain/Address.java
+++ src/main/java/com/barclays/eagle_bank_api/domain/Address.java
@@ -1,7 +1,9 @@
 package com.barclays.eagle_bank_api.domain;
 
 import jakarta.persistence.Embeddable;
+import java.io.Serializable;
 
 @Embeddable
 public record Address(
-    String line1, String line2, String line3, String town, String county, String postcode) {}
+    String line1, String line2, String line3, String town, String county, String postcode)
+    implements Serializable {}
